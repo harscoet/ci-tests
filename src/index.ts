79a75734fb04b6e1c8546949bfc1e81536773933
Harscoet Donovan <donovan.harscoet@gmail.com>
2017-05-17 11:12:24 +0200

    Fix lodash error

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import arrify = require('arrify');
 import { set } from 'lodash';
 
 const myObj = {};
-set(myObj, 'hello');
+set(myObj, 'hello', 'world');
 
 console.log(arrify(1));
 console.log(myObj);
